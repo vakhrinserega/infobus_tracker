d599d684a23535bae21874ef6ad5f7e49555174b
vakhrinserega <vakhrinserega@gmail.com>
2025-09-01 15:53:01 +0500

    Initial Commit

---
+++ Infobus Tracker/Infobus_TrackerApp.swift
@@ -0,0 +1,17 @@
+//
+//  Infobus_TrackerApp.swift
+//  Infobus Tracker
+//
+//  Created by Mag on 01.09.2025.
+//
+
+import SwiftUI
+
+@main
+struct Infobus_TrackerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Infobus TrackerUITests/Infobus_TrackerUITestsLaunchTests.swift
@@ -0,0 +1,33 @@
+//
+//  Infobus_TrackerUITestsLaunchTests.swift
+//  Infobus TrackerUITests
+//
+//  Created by Mag on 01.09.2025.
+//
+
+import XCTest
+
+final class Infobus_TrackerUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    @MainActor
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
